f4f9a759b064a44c0ed9c2d740c6a3ecba1dfcad
Adam Smith <zero1three@gmail.com>
2016-06-25 17:25:33 -0700

    rubocop location fact

--- puppet/facts/location.rb
+++ puppet/facts/location.rb
@@ -1,8 +1,8 @@
 require 'ipaddr'
 
-nyc      = IPAddr.new("10.133.0.0/16")
-san_fran = IPAddr.new("10.142.0.0/16")
-cloud    = IPAddr.new("10.10.10.0/24")
+nyc      = IPAddr.new('10.133.0.0/16')
+san_fran = IPAddr.new('10.142.0.0/16')
+cloud    = IPAddr.new('10.10.10.0/24')
 
 Facter.add('node_location') do
   node_location = 'Chicago'
@@ -12,18 +12,10 @@ Facter.add('node_location') do
 
     interfaces_array.each do |interface|
       ipaddress = Facter.value("ipaddress_#{interface}")
-      if nyc.include?(ipaddress)
-        node_location = 'NYC'
-      end
-      if san_fran.include?(ipaddress)
-        node_location = 'San Francisco'
-      end
-      if cloud.include?(ipaddress)
-        node_location = 'AWS'
-      end
-    end 
+      node_location = 'NYC' if nyc.include?(ipaddress)
+      node_location = 'San Francisco' if san_fran.include?(ipaddress)
+      node_location = 'AWS' if cloud.include?(ipaddress)
+    end
     node_location
   end
 end
-
-
